f39079fc69c70c386e07d51d8b92a7f498a0acb8
FranzJopper <124507237+FranzJopper@users.noreply.github.com>
2023-04-21 15:45:40 +0200

    Add files via upload

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -70,7 +70,7 @@ function simpleForwardFunc(accessToken) {
   // Details for formatting the URL can be found at
   // https://docs.microsoft.com/previous-versions/office/office-365-api/api/version-2.0/mail-rest-operations#get-messages.
   var forwardUrl = Office.context.mailbox.restUrl + "/v2.0/me/messages/" + itemId + "/forward";
-  console.log("bonjour")
+
   const forwardMeta = JSON.stringify({
     Comment: "FYI",
     ToRecipients: [
@@ -95,6 +95,15 @@ function simpleForwardFunc(accessToken) {
   });
 }
 
+
+/* Forward as Attachment */
+function forwardAsAttachment(){
+  Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function(result) {
+    var ewsId = Office.context.mailbox.item.itemId;
+    var accessToken = result.value;
+    forwardAsAttachmentFunc(accessToken);
+  });
+}
 
 /* Forward as Attachment */
 function forwardAsAttachment(){
