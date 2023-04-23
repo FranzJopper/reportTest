a6f80ec5b5da10813ba5b32f98883c2e007a46db
FranzJopper <124507237+FranzJopper@users.noreply.github.com>
2023-04-23 14:37:05 +0200

    Add files via upload

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -94,7 +94,6 @@ function simpleForwardFunc(accessToken) {
     sucessNotif("Email Forward successful!");
     //Note à moi même : Always permet de faire cette tache meme si on reussi, essaye de confirmer ça et donc de changer le code en conséquence
     // Supprimer le message électronique d'origine
-    
    
   });
 }
@@ -111,6 +110,7 @@ function confirmationSimpleForward() {
               function (args) {
                  if (args.message === "transferer") {
                     simpleForwardEmail();
+                    suppEmail();
                     dialog.close();
                  } else {
                     dialog.close();
