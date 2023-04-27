3dfab44b638ab516cf42d99d04e91d7f3f32d667
FranzJopper <124507237+FranzJopper@users.noreply.github.com>
2023-04-27 11:43:09 +0200

    Add files via upload

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -116,7 +116,7 @@ function simpleForwardFunc2(accessToken) {
   }).always(function (response) {
 
     var forwardItemId = response.id;
-    sucessNotif("Sujet du message transféré modifié avec succès 4 : " + itemId + " : " + forwardItemId);
+    sucessNotif("Sujet du message transféré modifié avec succès 10 : " + itemId + " : " + forwardItemId);
     
     var updateUrl = Office.context.mailbox.restUrl + "/v1.0/me/messages/" + forwardItemId;
 
