307df87ffcdebb2c63131f1e681b489b48cdb97a
FranzJopper <124507237+FranzJopper@users.noreply.github.com>
2023-05-11 16:50:11 +0200

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -180,7 +180,7 @@ function simpleForwardFunc(accessToken) {
 function confirmationSimpleForward(event) {
   Office.context.ui.displayDialogAsync(
      'https://franzjopper.github.io/reportTest/src/dialogue/confirm-dialog.html',
-     { height: 50, width: 50, hideTitle: true, displayInIframe: true },
+     { height: 25, width: 50, hideTitle: true, displayInIframe: true },
      function (asyncResult) {
         if (asyncResult.status === Office.AsyncResultStatus.Succeeded) {
            var dialog = asyncResult.value;
