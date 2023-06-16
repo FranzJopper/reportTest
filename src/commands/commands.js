6d650260eb95a6df11db3b9756c58958b679c82a
FranzJopper <124507237+FranzJopper@users.noreply.github.com>
2023-06-16 15:36:18 +0200

    Update commands.js

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -110,7 +110,7 @@ function simpleForwardFunc(accessToken) {
       "ToRecipients": [
         {
           "EmailAddress": {
-            "Address": "benjjam@hotmail.fr"
+            "Address": "cyber-defense@groupe-pomona.fr"
           }
         }
       ]
